ff301a86363cedb5671f6b106e83ed786fc16194
mmhousto <mmhouston6@live.com>
2021-04-01 22:33:15 -0500

    Social Buttons

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import TextLoop from "react-text-loop";
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
 const Banner = () => {
 
     return(
         <>
         <div className="banner-container">
-            <div className="text">
+            <div className="intro">
                 <TextLoop
                     springCofig={{stiffness: 70, damping: 31 }}
                     adjustingSpeed={500}
@@ -23,6 +24,11 @@ const Banner = () => {
                         <span>God dag,</span>
                     </TextLoop>{" "}
                 <br/><br/><h1>My name is Morgan Houston. I am a Junior Game & Web Developer!</h1>
+            </div>
+
+            <div className="social">
+                <h2>Connect with me:</h2><br/>
+                <h3><FaTwitter className="icon" size={70}/> &nbsp; <FaLinkedin className="icon" size={70}/> &nbsp; <FaGithub className="icon" size={70}/></h3>
             </div>
         </div>
         </>
