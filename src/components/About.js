00ff3334bcec1ebc0db962afb93918c988d7b62a
mmhousto <mmhouston6@live.com>
2021-04-28 21:59:04 -0500

    Revert "Github pages"
    This reverts commit 6804c94c59926d0c30249ce7114e83ef50ef3024.

---
+++ src/components/About.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Me from '../images/me.png';
+import Arrow from '../images/up-arrow-with-scribble.png';
+
+// Speech Bubble component
+const Bubble = (props) => {
+
+    // creates state for text to change in speech bubble and defines an array of text options to cycle through.
+    const [text, setText] = useState("I love video games! Whether it is playing or developing them. Diving into another world is so fascinating to me.");
+    const texts = [];
+    const [i, setI] = useState(1);
+    texts.push('I love video games! Whether it is playing or developing them. Diving into another world is so fascinating to me.');
+    texts.push('My top tech skills are JavaScript, C#, SQL, and node.js!');
+    texts.push('I am a highly motivated individual, who will stop at nothing until everything is perfect.');
+    texts.push('Interested in Software, Game, Mobile, and Web development opportunities.');
+    texts.push('React.JS is the bees knees! It is fast and you only need the basics - HTML, CSS, and JavaScript - and node.JS.');
+    texts.push('My first game I ever played was, Mario Party 2, on the N64. One of my favorite games is Borderlands 2.');
+    texts.push('I currently work in IT, for MCNC, as a Support Specialist. This allows my problem solving skills to continue to grow.');
+    texts.push('I am 23 years of age and love video games and soccer! Messi > Ronaldo');
+
+    // changes text to next string in array.
+    function changeText() {
+        setI(i + 1);                  // increases i
+        if (i >= texts.length - 1) {    // if i > lenght of array, sets i = 0
+            setI(0)
+        }
+        setText(texts[i]);          // Sets new text
+    }
+
+    return (
+        <>
+            <div className="bubble-container" onClick={changeText}>
+                <div className="bubble" >
+                    <p>
+                        {text}
+                    </p>
+                </div>
+                <div className="click-me">
+                    <img src={Arrow} alt="Up arrow Icon from Flaticon made by Freepik" />
+                    <div className="click-me-text">Click Here</div>
+                </div>
+                <div className="bubbles"></div>
+
+                <div className="bubbles2"></div>
+            </div>
+        </>
+    );
+}
+
+const About = () => {
+
+    useEffect(() => {
+        AOS.init({
+            duration: 1000
+        });
+    }, []);
+
+    return (
+        <>
+            <div id="About" className="about-container">
+                <div data-aos='slide-right'>
+                    <h1>About Me</h1>
+                </div>
+                <Bubble />
+                <div className="image-container">
+                    <img
+                        src={Me} alt="Picture of Morgan Houston, A Game & Web Developer"
+                    />
+                </div>
+
+            </div>
+        </>
+    );
+}
+
+export default About;
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+
+const Footer = () => {
+
+    return (
+        <>
+        <footer>
+            <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+            <div>Morgan Houston &copy; 2021</div>
+        </footer>
+        </>
+    );
+}
+
+export default Footer;
