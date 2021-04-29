cfd54ee0e4f49a523a7c5b9f9e33720b465ac08f
mmhousto <mmhouston6@live.com>
2021-04-28 22:26:35 -0500

    Updates

---
+++ precache-manifest.58f9e17553a446b2e0b430ce58735e7c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "153b3ee5f9caa7536592b07211483391",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "421f28cd00d940ad02f6",
+    "url": "/Portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "84bc2b86b05bf7823a32",
+    "url": "/Portfolio/static/css/main.5471176f.chunk.css"
+  },
+  {
+    "revision": "421f28cd00d940ad02f6",
+    "url": "/Portfolio/static/js/2.68ed5d5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.68ed5d5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84bc2b86b05bf7823a32",
+    "url": "/Portfolio/static/js/main.66366d63.chunk.js"
+  },
+  {
+    "revision": "efed155d89bc85b81984",
+    "url": "/Portfolio/static/js/runtime-main.88fabfc4.js"
+  },
+  {
+    "revision": "bd17debd43545504e5a76bf79403d04e",
+    "url": "/Portfolio/static/media/DDD.bd17debd.png"
+  },
+  {
+    "revision": "2e9ae583a72bbb07d001472f1667b73f",
+    "url": "/Portfolio/static/media/MMsGaming.2e9ae583.png"
+  },
+  {
+    "revision": "ca298c525b44d61f3cee9ba9f1593d8b",
+    "url": "/Portfolio/static/media/My-Stripes.ca298c52.jpg"
+  },
+  {
+    "revision": "9250971e99e1c427d428a0a439ea4e6e",
+    "url": "/Portfolio/static/media/Paddle-Balls.9250971e.png"
+  },
+  {
+    "revision": "7c339423d755c26c2cb81e7424e20430",
+    "url": "/Portfolio/static/media/Welcome2.7c339423.png"
+  },
+  {
+    "revision": "24d503fefb296cc533e09531b7b2f236",
+    "url": "/Portfolio/static/media/background.24d503fe.png"
+  },
+  {
+    "revision": "05cdaff4fa5988280bff064b0ad94fbe",
+    "url": "/Portfolio/static/media/me.05cdaff4.png"
+  }
+]);
