db145e89a992b5323e077b3368febd7ad6f1f482
Morgan Houston <mmhouston6@live.com>
2023-03-12 19:46:03 -0500

    Added ZombCube and Magnet Destroyer

--- src/components/Work.js
+++ src/components/Work.js
@@ -61,6 +61,33 @@ const Work = () => {
                 <br /><br />
 
                 <Project
+                    title="ZombCube"
+                    description={"Play ZombCube, a 3D fps, zombie-survival game for free online on PC, Xbox, MacOS, tvOS, iOS, and Android.\n\nCreated with the Unity Engine"}
+                    image={ZombCube}
+                    url="https://mms-gaming.com/ZombCube/"
+                    repo="https://github.com/mmhousto/ZombCube"
+                />
+                <br /><br />
+
+                <Project
+                    title="Paddle Balls"
+                    description={"A native mobile arcade game, like pong with gravity!\nAvailable on iOS, Android and PC.\n\nCreated with Unity Engine and C#"}
+                    image={PaddleBalls}
+                    url="https://mms-gaming.com/Gravity-Balls/"
+                    repo="https://github.com/mmhousto/Gravity-Balls"
+                />
+                <br /><br />
+
+                <Project
+                    title="Magnet Destroyer"
+                    description={"Play this new, free, casual, arcade game, Magnet Destroyer! Available on iOS and Android.\n\nCreated with Unity and Sloyd.ai"}
+                    image={magDestroyer}
+                    url="https://mms-gaming.com/Magnet-Destroyer/"
+                    repo="https://github.com/mmhousto/Magnet-Destroyer"
+                />
+                <br /><br />
+
+                <Project
                     title="MM's Gaming Community"
                     description={"A place for gamers to play free online games and discuss gaming! If they have an account, as they play they will earn points. Gamers can use the points to get colored backgrounds, image backgrounds, circle trails, fonts, and custom cursors.\n\nCreated with XAMPP, MySQL, HTML5, CSS3, JavaScript, Ajax and PHP"}
                     image={MMsGCo}
@@ -68,6 +95,7 @@ const Work = () => {
                     repo="https://github.com/mmhousto/MMsGaming"
                 />
                 <br /><br />
+
                 <Project
                     title="My Stripes"
                     description={"An e-commerce website for a clothing app. A remake of the Ruby on Rails version.\n\nCreated with Postgres, Express, React, Node.JS, SCSS, Heroku and JavaScript"}
@@ -76,14 +104,7 @@ const Work = () => {
                     repo="https://github.com/zda17/MyStripesShop---API"
                 />
                 <br /><br />
-                <Project
-                    title="Paddle Balls"
-                    description={"A native mobile arcade game, like pong with gravity!\nAvailable on iOS, Android and PC.\n\nCreated with Unity Engine and C#"}
-                    image={PaddleBalls}
-                    url="https://apps.apple.com/us/app/paddle-balls/id1549834349"
-                    repo="https://github.com/mmhousto/Gravity-Balls"
-                />
-                <br /><br />
+                
                 <Project
                     title="Del Delivery Drivers"
                     description={"A web app, created to compare the prices of different delivery services.\n\nCreated with XAMPP, Apache, MySQL, HTML5, CSS3, JavaScript, PHP, and Google Maps API"}
